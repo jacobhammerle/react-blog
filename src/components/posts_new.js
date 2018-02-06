4975a46d21da1dfd0233d78930659281809b6972
Jacob Hammerle <hammerja@miamioh.edu>
2018-02-06 11:13:23 -0500

    extra validation and navigation back home

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,23 +1,36 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
 
 class PostsNew extends Component {
 	renderField(field) {
+		const { meta: { touched, error } } = field;
+		const className = `form-group ${touched && error ? 'has-danger' : ''}`;
+
 		return (
-			<div>
+			<div className={className}>
 				<label>{field.label}</label>
 				<input 
 					className="form-control"
 					type="text"
 					{...field.input}
 				/>
+				<div className="text-help">
+					{touched ? error : ''}
+				</div>
 			</div>
 		)
 	}
 
+	onSubmit(values) {
+		console.log(values);
+	}
+
 	render() {
+		const { handleSubmit } = this.props;
+
 		return (
-			<form>
+			<form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
 				<Field
 					label="Title"
 					name="title"
@@ -33,6 +46,12 @@ class PostsNew extends Component {
 					name="content"
 					component={this.renderField}
 				/>
+				<button type="submit" className="btn btn-primary">
+					Submit
+				</button>
+				<Link className="btn btn-danger" to="/">
+					Cancel
+				</Link>
 			</form>
 		)
 	}
