b43950ba6cdb0bd7a68d36885dcdc9ffe1d7467a
Jacob Hammerle <hammerja@miamioh.edu>
2018-02-08 17:58:58 -0500

    added front end individual post structure, loading check, and navigation

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { fetchPost } from '../actions';
 
 class PostsShow extends Component {
+	componentDidMount() {
+		const { id } = this.props.match.params;
+		this.props.fetchPost(id);
+	}
+
 	render() {
+		const { post } = this.props;
+
+		console.log({post});
+
+		if (!post) {
+			return <div>Loading...</div>;
+		}
+
+		// the post we want to show
+		// posts[this.props.match.params.id];
 		return (
 			<div>
-				Posts Show
+				<Link to='/'>Back to index</Link>
+				<h3>{post.title}</h3>
+				<h6>Categories: {post.categories}</h6>
+				<p>{post.content}</p>		
 			</div>
 		)
 	}
 }
 
-export default PostsShow
+function mapStateToProps({ posts }, ownProps) {
+	return { post: posts[ownProps.match.params.id] };
+}
+
+export default connect(null, { fetchPost })(PostsShow);
